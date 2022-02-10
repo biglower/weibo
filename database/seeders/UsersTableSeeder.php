c971c8c445502ea17b7c7bf517cea5c09d282dfa
shixiangwei <shixiangwei_php@163.com>
2022-02-10 07:46:16 +0000

    管理员可删除用户

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -19,6 +19,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'Shixw';
         $user->email = 'shixiangwei_php@163.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
